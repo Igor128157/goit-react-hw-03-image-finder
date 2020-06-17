5b921409281d0d956433c7c58560877804ed08e7
Igor Verovka <igor0063@ukr.net>
2020-06-17 21:14:56 +0300

    Updates

---
+++ precache-manifest.f40b4244913ac3ebf03ef0e0b43346f4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "775a64341fd0b01d48aca7bb8379a55b",
+    "url": "/goit-react-hw-03-image-finder/index.html"
+  },
+  {
+    "revision": "0dff772398c648648b13",
+    "url": "/goit-react-hw-03-image-finder/static/css/main.2a365386.chunk.css"
+  },
+  {
+    "revision": "3257090d8a43e9ed0308",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.464bf636.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.464bf636.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0dff772398c648648b13",
+    "url": "/goit-react-hw-03-image-finder/static/js/main.90a7b5b7.chunk.js"
+  },
+  {
+    "revision": "00ed64b4697a879a9e95",
+    "url": "/goit-react-hw-03-image-finder/static/js/runtime-main.4e03020b.js"
+  }
+]);
